ebf594c475911e4d6b8a42ff6cf134e185fc89ca
SRIKANTH690 <selvamsrikanth135@gmail.com>
2026-05-26 14:41:24 +0530

    Time: 1 ms (100%), Space: 43.6 MB (81.71%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -0,0 +1,25 @@
+// class Solution {
+//     public int numberOfSpecialChars(String word) {
+//         int c  = 0;
+
+//         for(char ch ='a' ;ch <= 'z' ;ch++){
+//             if(word.indexOf(ch) != -1 && word.indexOf(Character.toUpperCase(ch) != -1) ){
+//                 c++;
+//             }
+//         }
+//         return c;
+        
+//     }
+// }
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int count = 0;
+        for(char ch = 'a'; ch <= 'z'; ch++) {
+            if(word.indexOf(ch) != -1 &&
+               word.indexOf(Character.toUpperCase(ch)) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
