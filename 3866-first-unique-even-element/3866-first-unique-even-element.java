aed6de3dda0431c3cf37e595d7d7f2dc74eb7a03
SRI KANTH S <selvamsrikanth135@gmail.com>
2026-09-03 14:24:46 +0530

    Time: 1 ms (99.25%), Space: 46.4 MB (33.95%) - LeetHub

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+        int[] freq = new int[101];
+
+        for(int i = 0; i < nums.length; i++) {
+            freq[nums[i]]++;
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] % 2 == 0 && freq[nums[i]] == 1) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}
