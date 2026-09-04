8ce0aa304d728a52ee1c1e56c1fff9501913a965
SRI KANTH S <selvamsrikanth135@gmail.com>
2026-09-04 21:01:26 +0530

    Time: 0 ms (100%), Space: 44.8 MB (6.12%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,18 +1,36 @@
+// class Solution {
+//     public int maximumWealth(int[][] accounts) {
+//         int max = 0;
+//         for(int i=0;i<accounts.length;i++){
+//             int sum = 0;
+
+//             for(int j = 0;j<accounts[i].length;j++){
+//                 sum += accounts[i][j];
+//             }
+
+//             if(sum > max){
+//                 max = sum;
+//             }
+//         }
+//         return max;
+        
+//     }
+// }
+
+
 class Solution {
     public int maximumWealth(int[][] accounts) {
         int max = 0;
-        for(int i=0;i<accounts.length;i++){
+
+        for(int[] c : accounts){
             int sum = 0;
 
-            for(int j = 0;j<accounts[i].length;j++){
-                sum += accounts[i][j];
+            for(int money : c){
+                sum += money;
             }
 
-            if(sum > max){
-                max = sum;
-            }
+            max = Math.max(sum , max);
         }
         return max;
-        
     }
 }
